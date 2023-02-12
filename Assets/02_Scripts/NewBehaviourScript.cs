63124c85d68b0d1bc18e9de08abb25fb0efbb51f
leewnstj <ij35492581@gmail.com>
2023-02-12 22:48:22 +0900

    cam

---
+++ Assets/02_Scripts/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] Transform target1;
+    [SerializeField] Transform target2;
+
+    [SerializeField] Camera cam;
+    [SerializeField] float smoothing;
+
+    public float x;
+
+    private void Start()
+    {
+        x = Camera.main.orthographicSize;
+    }
+
+    private void Update()
+    {
+        Vector3 camPos = new Vector3((target1.position.x + target2.position.x) / 2, (target1.position.y + target2.position.y) / 2 + 3, -10);
+        cam.transform.position = Vector3.Lerp(cam.transform.position, camPos, smoothing);
+    }
+
+
+}
